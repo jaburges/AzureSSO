0ecd3d940005c29435187a82e1c64647c89e4576
Jamie Burgess <j.burgess@reply.com>
2025-12-10 10:57:48 -0800

    0.1.3 - cleaned up

--- team-members/inc/tmm-text-domain.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-/* Loads plugin's text domain. */
-add_action( 'plugins_loaded', 'tmm_load_plugin_textdomain' );
-function tmm_load_plugin_textdomain() {
-  load_plugin_textdomain( 'team-members', false, plugin_basename( TMM_PATH . 'lang' ) );
-}
